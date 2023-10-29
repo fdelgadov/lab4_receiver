6dc3bd807137d364958c04a3fefd467040a306db
fdelgadov <fdelgadov@unsa.edu.pe>
2023-10-28 21:08:17 -0500

    app con interfaz

--- app/src/main/java/org/idnp/idnp2022lab04/MainActivity.java
+++ app/src/main/java/org/idnp/idnp2022lab04/MainActivity.java
@@ -6,22 +6,35 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     final private String TAG = "App1";
+
+    private EditText et_msg;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        et_msg = findViewById(R.id.tbx_msg);
+        Button btn = findViewById(R.id.button);
 
         IntentFilter filter = new IntentFilter("com.example.APP1_ACTION");
         MyBroadcastReceiver mbr = new MyBroadcastReceiver();
         registerReceiver(mbr, filter);
+    }
+
+    public void enviarMensaje(View view){
+        String msg = et_msg.getText().toString();
 
         Intent intent = new Intent("com.example.APP1_ACTION");
-        intent.putExtra("key", "App1"); // Puedes poner datos extras si es necesario
+        intent.putExtra("key", msg); // Puedes poner datos extras si es necesario
 
         sendBroadcast(intent);
         Log.d(TAG, "Mensaje enviado");
+
+        et_msg.setText("");
     }
 }
